7423a2e0cc7c4e3712c416306c5392fdfaf48f60
Mohamad Supangat <hantamkoding@gmail.com>
2022-03-28 15:05:13 +0700

    update quote Soul Evans

--- update.js
+++ update.js
@@ -6,18 +6,15 @@ const { exec } = require("child_process");
 fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(quote => {
-      const new_quote = `#${quote.anime}
-##${quote.character}
+      const new_quote = `# ${quote.anime}
+## ${quote.character}
 
 ${quote.quote}`;
-
-      console.log(new_quote);
-
       try {
         fs.writeFileSync('README.md', new_quote);
         exec("git add .");
         exec(`git commit -m 'update quote ${quote.character}'`);
-
+        exec(`git push origin master`);
       } catch (err) {
         console.error(err)
       }
