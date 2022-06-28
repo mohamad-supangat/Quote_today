e2aabf61ee0ad0535ca5dfd286c2a4c0c283056c
Mohamad Supangat <hantamkoding@gmail.com>
2022-06-29 01:54:03 +0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Gunslinger Stratos: The Animation
 
-Tooru Kazasumi
+<p align="center">
+  <img width="500" src="https://nekos.best/api/v2/neko/0645.png">
 
-It has happened many times throughout history. Resources, territory, wealth... Humans fight each other in order to gain something they don't have. It's simple. How can you be concerned with the lives of others when you're about to starve to death? You're too naive. Humans can only live by stealing what they don't have from others. If there's no future, we just take the future from others.
+
+  ### <a href="https://www.pixiv.net/en/artworks/94542906">時雨しずく</a>
+</p>
+
--- update.js
+++ update.js
@@ -3,18 +3,28 @@ const fs = require("fs");
 const { exec } = require("child_process");
 const getEmoji = require("get-random-emoji");
 
-fetch("https://animechan.vercel.app/api/random")
+fetch("https://nekos.best/api/v2/neko")
   .then((response) => response.json())
-  .then((quote) => {
-    const new_quote = `# ${quote.anime}
+  .then((response) => {
+    let new_quote = "";
 
-${quote.character}
+    for (const anime of response.results) {
+      new_quote += `
+<p align="center">
+  <img width="500" src="${anime.url}">
 
-${quote.quote}`;
+
+  ### <a href="${anime.source_url}">${anime.artist_name}</a>
+</p>
+
+`;
+    }
+
+    // console.log(new_quote);
     try {
       fs.writeFileSync("README.md", new_quote);
       exec("git add .");
-      exec(`git commit -m '${getEmoji()} update quote ${quote.character}'`);
+      exec(`git commit -m '${getEmoji()}`);
       exec(`git push origin master`);
     } catch (err) {
       console.error(err);
