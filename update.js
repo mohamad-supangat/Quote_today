b5abb7306b9c6f5134124c9b4f099686d446bd64
Mohamad Supangat <hantamkoding@gmail.com>
2022-09-09 15:22:17 +0700

    🍚

--- update.js
+++ update.js
@@ -1,9 +1,11 @@
 const fetch = require("node-fetch");
+const HttpsProxyAgent = require('https-proxy-agent');
 const fs = require("fs");
 const { exec } = require("child_process");
 const getEmoji = require("get-random-emoji");
+const proxyAgent = new HttpsProxyAgent("http://106.75.171.235:8080");
 
-fetch("https://nekos.best/api/v2/neko")
+fetch("https://nekos.best/api/v2/neko", { agent: proxyAgent})
   .then((response) => response.json())
   .then((response) => {
     let new_quote = "";
